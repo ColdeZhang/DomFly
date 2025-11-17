453c776d36dbc923a531a0a40ee91d474f9348dd
wunan <wunanc@163.com>
2025-11-17 10:31:08 +0800

    First

---
+++ src/main/java/com/wunanc/domFly/DomFly.java
@@ -0,0 +1,17 @@
+package com.wunanc.domFly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DomFly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
